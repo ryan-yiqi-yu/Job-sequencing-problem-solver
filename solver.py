0413fc72ead1be5f7ae43f6be20bee966a426221
Raghav Singh <rakeshsingh@Rakeshs-MacBook-Air.local>
2021-12-02 14:47:15 -0800

    updated solver.py

--- solver.py
+++ solver.py
@@ -8,13 +8,21 @@ def solve(tasks):
     Returns:
         output: list of igloos in order of polishing  
     """
+    return [1]
     pass
 
 
 # Here's an example of how to run your solver.
 # if __name__ == '__main__':
-#     for input_path in os.listdir('inputs/'):
-#         output_path = 'outputs/' + input_path[:-3] + '.out'
-#         tasks = read_input_file(input_path)
-#         output = solve(tasks)
-#         write_output_file(output_path, output)
+#     for size in os.listdir('inputs/'):
+#         if size not in ['small', 'medium', 'large']:
+#             continue
+#         for input_file in os.listdir('inputs/{}/'.format(size)):
+#             if size not in input_file:
+#                 continue
+#             input_path = 'inputs/{}/{}'.format(size, input_file)
+#             output_path = 'outputs/{}/{}.out'.format(size, input_file[:-3])
+#             print(input_path, output_path)
+#             tasks = read_input_file(input_path)
+#             output = solve(tasks)
+#             write_output_file(output_path, output)
